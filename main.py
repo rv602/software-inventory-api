7991ee95e6597830705c4345cbc07f81edd30f8a
Rajagopalan <rajagopalan602@gmail.com>
2023-10-17 12:28:44 +0530

    fixed cors error

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import subprocess
 import json
@@ -10,6 +11,22 @@ import json
 app = FastAPI()
 #load environment variables
 load_dotenv()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
